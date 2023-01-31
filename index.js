15346fe7954a0d369e0f08cad171e9219192f023
JollyR0ger <kav993@gmail.com>
2023-01-31 14:27:39 +0200

    add cors

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
+const cors = require('cors')
+
 const port = process.env.PORT || 3000
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200,
+}
+
+const app = express()
+
+app.use(cors(corsOptions))
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.post('/', (req, res) => {
-  console.log('POST', req, res)
+  console.log('----')
+  console.log('POST', req.body)
 })
 
 app.listen(port, () => {
