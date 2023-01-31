ae7c9b84640a6625f6852715303360d156f1bb7a
JollyR0ger <kav993@gmail.com>
2023-01-31 15:22:56 +0200

    add cookies

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 
 const port = process.env.PORT || 3000
 const corsOptions = {
   origin: 'http://localhost:3000',
   optionsSuccessStatus: 200,
+  credentials: true,
 }
 
 const app = express()
 
 app.use(cors(corsOptions))
+app.use(cookieParser())
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -19,7 +22,10 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   console.log('----')
+  console.log('Cookies:', req.cookies)
   console.log('POST', req.body)
+  res.append('Set-Cookie', 'token=foobar; HttpOnly;')
+  res.send()
 })
 
 app.listen(port, () => {
