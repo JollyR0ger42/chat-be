20cfa1790897a9667dacf33eb5bccfe9d7ce2cab
JollyR0ger <kav993@gmail.com>
2023-02-02 17:10:02 +0200

    minor changes

--- controller/users.js
+++ controller/users.js
@@ -2,17 +2,17 @@ const token = require('../src/token.js')
 
 module.exports = (collection) => {
   async function addUser(login, password) {
-    // console.log('Args:', login, password)
-    console.log('addUser', login, password)
     let result
+
     try {
       result = await collection.insertOne({ login, password })
-      if (result)
-        return token.generate({login})
     } catch (e) {
       console.log(e)
     }
+
+    if (result)
+      return token.generate({ login })
   }
 
-  return {addUser}
+  return { addUser }
 }
